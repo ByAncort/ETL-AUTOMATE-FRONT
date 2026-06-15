c526dabfcf87f997cdfb7ca1fad802607c98b890
ByAncort <diego2005cortes@gmail.com>
2026-06-14 22:53:30 -0400

    chore: agregar configuracion de Jest y dependencias de testing

---
+++ jest.config.cjs
@@ -0,0 +1,36 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx', '**/*.test.ts', '**/*.test.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/main.tsx',
+    '!src/**/vite-env.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  coverageReporters: ['text', 'lcov', 'json-summary'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.app.json',
+    }],
+  },
+  setupFiles: [],
+  clearMocks: true,
+  restoreMocks: true,
+};
+
+module.exports = config;
